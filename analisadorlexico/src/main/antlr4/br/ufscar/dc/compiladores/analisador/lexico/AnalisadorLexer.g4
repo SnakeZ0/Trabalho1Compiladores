c7c66c71c6bee05e5308732b0c31ee1626ba28a8
PedroB <72228482+SnakeZ0@users.noreply.github.com>
2023-05-30 19:49:41 -0300

    AnalisadorLexico Primeira versão
    falta rodar testes e tratar erros de forma mais correta

--- analisadorlexico/src/main/antlr4/br/ufscar/dc/compiladores/analisador/lexico/AnalisadorLexer.g4
+++ analisadorlexico/src/main/antlr4/br/ufscar/dc/compiladores/analisador/lexico/AnalisadorLexer.g4
@@ -1,6 +1,42 @@
 lexer grammar AnalisadorLexer;
 
-Letra		:	'a'..'z' | 'A'..'Z';
-Digito	:	'0'..'9';
-Variavel	:	Letra(Letra|Digito)* { System.out.print("[Var,"+getText()+"]");};
 
+PALAVRA_CHAVE 
+	:	'declare' | 'algoritmo' | 'inteiro' | 'literal' | 'leia' | 'escreva' | 'fim_algoritmo' 	; 
+NUMINT	: ('+'|'-')?('0'..'9')+
+	;
+NUMREAL	: ('+'|'-')?('0'..'9')+ ('.' ('0'..'9')+)?
+	;
+IDENT : ('a'..'z'|'A'..'Z') ('a'..'z'|'A'..'Z'|'0'..'9')*
+	 ;
+CADEIA  : '"' ( ESC_SEQ | ~('"'|'\\') )* '"';
+fragment
+ESC_SEQ	: '\\"';
+COMENTARIO
+    :   '{' ~[\r\n]* '}' {skip();}
+    ;
+WS  :   ( ' '
+        | '\t'
+        | '\r'
+        | '\n'
+        ) {skip();}
+    ;
+OP_REL	:	'>' | '>=' | '<' | '<=' | '<>' | '='
+	;
+OP_ARIT	:	'+' | '-' | '*' | '/'
+	;
+DELIM	:	':'
+	;
+ABREPAR :	'('
+	;
+FECHAPAR:	')'
+	;
+VIR: ',' 
+	;
+ERRO: .
+	;
+CADEIA_LITERAL_NAO_FECHADA:  ('\'' | '"') (ESC_SEQ | ~('\n'|'\''|'\\'|'"'))* '\n'
+	;
+COMENTARIO_NAO_FECHADO: '{' ~('}' | '\n' | '\r')* ('\r' | '\n')
+	;
+ 
--- analisadorlexico/src/main/java/br/ufscar/dc/compiladores/analisador/lexico/Principal.java
+++ analisadorlexico/src/main/java/br/ufscar/dc/compiladores/analisador/lexico/Principal.java
@@ -1,5 +1,7 @@
 package br.ufscar.dc.compiladores.analisador.lexico;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
@@ -12,10 +14,46 @@ public class Principal {
             // args[0] é o primeiro argumento da linha de comando
             CharStream cs = CharStreams.fromFileName(args[0]);
             AnalisadorLexer lex = new AnalisadorLexer(cs);
-            while (lex.nextToken().getType() != Token.EOF) {
-                System.out.println("");
+            Token t = null;
+
+            // Verificar se o diretório de saída existe, criar se necessário
+            File outputFile = new File(args[1]);
+            File outputDir = outputFile.getParentFile();
+            if (outputDir != null && !outputDir.exists()) {
+                outputDir.mkdirs();
             }
+
+            FileWriter writer = new FileWriter(outputFile);
+            String textoarq;
+
+            while ((t = lex.nextToken()).getType() != Token.EOF) {
+                String tipo = AnalisadorLexer.VOCABULARY.getDisplayName(t.getType());
+                String texto = t.getText();
+
+                if (tipo.equals("ERRO")) {
+                    textoarq = "Linha " + t.getLine() + ": erro - Comentário não fechado\n";
+                    writer.write(textoarq);
+                    break;
+                } else if (tipo.equals("CADEIA__LITERAL_NAO_FECHADA")) {
+                    textoarq = "Linha " + t.getLine() + ": erro - Cadeia literal não fechada\n";
+                    writer.write(textoarq);
+                    break;
+                } else if (tipo.equals("COMENTARIO_NAO_FECHADO")) {
+                    textoarq = "Linha " + t.getLine() + ": erro - Comentário não fechado\n";
+                    writer.write(textoarq);
+                    break;
+                } else if (tipo.equals("PALAVRA_CHAVE")) {
+                    tipo = "'" + texto.toLowerCase() + "'";
+                }
+
+                textoarq = "<'" + texto + "'," + tipo + ">\n";
+                writer.write(textoarq);
+            }
+
+            writer.close();
+
         } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 }
